4e80e3bc3eb3119949ffb3cfe86afa438fba1da8
Annika Melin <melin.anki>
2020-09-29 16:53:50 +0200

    image rating and cost added

--- script.js
+++ script.js
@@ -6,27 +6,36 @@ const apiUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=74&entit
 fetch(apiUrl, { headers: { "user-key": apiKey } })
   .then(response => response.json())
   .then(json => {
+    console.log(json)
     const myRestaurants = json.restaurants.map(item => {
       name = item.restaurant.name
       address = item.restaurant.location.address
-      cost = item.restaurant.average_cost_for_two
+      cost = new Number(item.restaurant.average_cost_for_two).toLocaleString("se-SE")
       rating = item.restaurant.user_rating.aggregate_rating
       image = item.restaurant.featured_image
       return { name, cost, address, rating, image }
     })
     console.log(myRestaurants)
+    console.log(myRestaurants[0].cost)
+
     myRestaurants.forEach(item => {
       restaurants.innerHTML += generateRestaurant(item)
     })
   })
+
+
 
 // function that generates HTML for a restaurant. 
 const generateRestaurant = item => `
  <div class="restaurant-box"> 
   <h3 class="restaurant-name">${item.name}</h3> 
   <p class="restaurant-address">${item.address}</p>
+  <p class="restaurant-rating">${item.rating}</p>
+  <p class="restaurant-cost">${item.cost} IDR</p>
   <img class="restaurant-image" src="${item.image}">
 </div>`
+
+
 
 
 
