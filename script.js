d6a27ef1bcc3bef460a555153a292c31810920e8
Annika Melin <melin.anki>
2020-09-29 16:27:33 +0200

    added image

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ const apiUrl = 'https://developers.zomato.com/api/v2.1/search?entity_id=74&entit
 fetch(apiUrl, { headers: { "user-key": apiKey } })
   .then(response => response.json())
   .then(json => {
-    console.log(json.restaurants[0].restaurant.average_cost_for_two)
-    console.log(json.restaurants[0].restaurant.user_rating.aggregate_rating)
-
     const myRestaurants = json.restaurants.map(item => {
       name = item.restaurant.name
-      cost = item.restaurant.average_cost_for_two
       address = item.restaurant.location.address
+      cost = item.restaurant.average_cost_for_two
       rating = item.restaurant.user_rating.aggregate_rating
-      return { name, cost, address, rating }
+      image = item.restaurant.featured_image
+      return { name, cost, address, rating, image }
     })
-
+    console.log(myRestaurants)
     myRestaurants.forEach(item => {
       restaurants.innerHTML += generateRestaurant(item)
     })
@@ -27,6 +25,7 @@ const generateRestaurant = item => `
  <div class="restaurant-box"> 
   <h3 class="restaurant-name">${item.name}</h3> 
   <p class="restaurant-address">${item.address}</p>
+  <img class="restaurant-image" src="${item.image}">
 </div>`
 
 
